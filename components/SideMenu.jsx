bcc9a0e1536de81eb7b6815a50f986b2dcc91627
Gabriel <gabrielmenezes_@hotmail.com>
2024-05-19 22:55:03 -0400

    adicionando msg de direitos autorais

--- components/SideMenu.jsx
+++ components/SideMenu.jsx
@@ -83,6 +83,7 @@ const SideMenu = () => {
             })}
           </div>
           <div className="flex items-center flex-col justify-center text-center">
+            <p className="max-lg:hidden text-xs">Copyright © 2024. All rights reserved.</p>
           </div>
         </div>
       </section>
