03d66052b363480879d3b62c8f461dd7dbd49d45
Gabriel Menezes <gabrielmenezes_@hotmail.com>
2024-05-20 22:44:56 -0400

    adicionando imagem naa home

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,18 +2,25 @@
 import Image from "next/image";
 import Link from "next/link";
 import "@/app/animations.css";
+import Avatar from "./Avatar";
 
 const Hero = () => {
   return (
     <section className="hero-container">
+            <div className="hero-content-container">
         <div className="max-md:order-2 flex flex-col gap-5 z-40">
-          <div className="text-4xl lg:text-6xl">
+          <div className="text-4xl lg:text-6xl ">
             <h1 className="">Olá, eu sou <b className=" text-dark-red">Gabriel</b></h1>
+
+            <div className="flex justify-center items-center  pt-5 lg:hidden">
+            <Image src="/assets/home.jpg" width={200} height={200} alt="Gabriel Menezes" className="z-10 rounded-full" />
+
+            </div>
             <p className="mt-5 text-xl">
             Bacharel em ciência da computação, cursando especialização em qualidade e teste de software,
             com mais de 5 anos de experiência com automação, testes acústicos e teste de software Android.
             </p>
-          </div>
+          </div>  
 
           <div className="flex gap-4 max-md:justify-center">
             <Link href="#about">
@@ -28,6 +35,13 @@ const Hero = () => {
             </Link>
           </div>
       </div>
+
+      <div className="hidden lg:flex pl-8 ">
+           <Image src="/assets/home.jpg" width={800} height={800} alt="Gabriel Menezes" className="z-10 rounded-full" />
+      </div>
+
+          </div>
+
 
       <ul className="bg-squares">
         <li></li>
