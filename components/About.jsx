88ad9f0073b49958d1b5034acdfb3d2d04205d2b
Gabriel Menezes <gabrielmenezes_@hotmail.com>
2024-05-21 22:37:53 -0400

    about para dispositivos móveis + MD está pronto

--- components/About.jsx
+++ components/About.jsx
@@ -3,6 +3,69 @@ import Image from "next/image";
 import Link from "next/link";
 const About = () => {
   return (
+<section className="w-full h-fit px-[40px] py-10 relative bg-white md:grid md:grid-cols-2 md:items-center md:text-left" id="about">
+
+<div>
+      <p className="mt-6 text-gray-900 text-[20px] leadiing-10">
+        <b className="ml-5 flex">Mas e fora do trabalho?</b>
+      </p>
+        <ul class=" justify-center items-center mt-1 list-disc list-inside text-[20px] ">
+          <li>Piloto de drone</li>
+          <li>Simuleiro de aviação</li>
+          <li>Streamer</li>
+          <li>Aventureiro/adoro fazer trilhas</li>
+          <li>Jogador de vôlei</li>
+          </ul>
+          <p className="mt-6 text-gray-900 text-[20px] leadiing-10">
+            Ah! e não posso deixar de citar que participo das melhores comunidades desse planeta:
+          </p>
+          <ul class=" justify-center items-center mt-1 list-disc list-inside text-[20px] ">
+          <li>Python Norte &#128013;</li>
+          <li>Python Brasil &#x1F1E7;&#x1F1F7;</li>
+          <li>Pokémon</li>
+          </ul>
+    </div>
+
+    <div className="grid justify-center items-center">
+      <div className="md:grid grid-cols-2 ">
+        <div className="md:mr-3">
+        <Image src="/assets/home.jpg" width={300} height={300} alt="Gabriel Menezes" className="rounded-full" />
+        <p className="text-center">Legenda 1</p>
+        </div>
+        <div className="md:ml-3">
+        <Image src="/assets/home.jpg" width={300} height={300} alt="Gabriel Menezes" className="rounded-full" />
+        <p className="text-center">Legenda 1.1</p>
+        </div>
+      </div>
+
+      <div className="md:grid grid-cols-2 ">
+        <div className="md:mr-3">
+          <Image src="/assets/home.jpg" width={300} height={300} alt="Gabriel Menezes" className="rounded-full" />
+          <p className="text-center">Legenda 1</p>
+        </div>
+        <div className="md:ml-3">
+          <Image src="/assets/home.jpg" width={300} height={300} alt="Gabriel Menezes" className="rounded-full" />
+          <p className="text-center">Legenda 1.1</p>
+        </div>
+      </div>
+      
+      
+    </div>
+    
+
+
+
+
+
+
+
+</section>
+
+
+
+
+/*
+
 <section className="w-full h-fit px-[40px] py-10 relative bg-white" id="about">
   <div>
     <div className="inline-block">
@@ -61,6 +124,8 @@ const About = () => {
     </div>
   </div>
     </section>
+
+*/
   );
 };
 
