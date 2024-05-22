7c7c4feb884850a8242fb541c28a2259626ebcde
Gabriel Menezes <gabrielmenezes_@hotmail.com>
2024-05-21 21:10:47 -0400

    about para dispositivos móveis está pronto

--- components/About.jsx
+++ components/About.jsx
@@ -1,33 +1,65 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 const About = () => {
   return (
-    <section className="w-full h-fit px-[40px] py-10 max-xs:px-[20px] relative bg-white" id="about">
-      <div className="max-w-[1250px] mx-auto">
-        <div className="inline-block">
-          <p className="section-title">
-            <span className="mb-1">
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M8 0 L16 8 L8 16 L0 8 L8 0Z" fill="currentColor"></path>
-              </svg>
-            </span>
-            Quem sou eu ?
-          </p>
-        </div>
-        <div className="mt-7">
-          <p className="mt-6 text-gray-900 text-[20px] leadiing-10">
-            <b>Mas e fora do trabalho?</b>
-          </p>
-          <ul class="mt-2 list-disc list-inside text-[20px] leadiing-10">
-                <li>Piloto de drone</li>
-                <li>Simuleiro de aviação</li>
-                <li>Streamer</li>
-                <li>Membro da melhor comunidade de programação ~ &#x1F40D; Python Norte &#x1F40D;~</li>
-                <li>Participante da comunidade de Pokemon Manaus.</li>
+<section className="w-full h-fit px-[40px] py-10 relative bg-white" id="about">
+  <div>
+    <div className="inline-block">
+    <p className="section-title">
+          <span className="mb-1">
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M8 0 L16 8 L8 16 L0 8 L8 0Z" fill="currentColor"></path>
+            </svg>
+          </span>
+          Sobre mim
+        </p>
+    </div>
+    <div>
+      <p className="mt-6 text-gray-900 text-[20px] leadiing-10">
+        <b className="ml-5 flex">Mas e fora do trabalho?</b>
+      </p>
+        <ul class=" justify-center items-center mt-1 list-disc list-inside text-[20px] ">
+          <li>Piloto de drone</li>
+          <li>Simuleiro de aviação</li>
+          <li>Streamer</li>
+          <li>Aventureiro/adoro fazer trilhas</li>
+          <li>Jogador de vôlei</li>
           </ul>
           <p className="mt-6 text-gray-900 text-[20px] leadiing-10">
-             &#x1F604;
+            Ah! e não posso deixar de citar que participo das melhores comunidades desse planeta:
           </p>
-        </div>
+          <ul class=" justify-center items-center mt-1 list-disc list-inside text-[20px] ">
+          <li>Python Norte &#128013;</li>
+          <li>Python Brasil &#x1F1E7;&#x1F1F7;</li>
+          <li>Pokémon</li>
+          </ul>
+    </div>
+
+    <div>
+
+      <div className="m-4  grid grid-cols-2">
+        <Image src="/assets/home.jpg" width={200} height={150} alt="Gabriel Menezes" className="z-10 rounded-full mt-4" />
+        <p className="text-[20px] flex justify-center items-center">legenda 1</p>
       </div>
+
+      <div className="m-4  grid grid-cols-2">
+      <p className="text-[20px] flex justify-center items-center">legenda 2</p>
+        <Image src="/assets/home.jpg" width={200} height={150} alt="Gabriel Menezes" className="z-10 rounded-full mt-4" />
+      </div>
+
+      <div className="m-4  grid grid-cols-2">
+        <Image src="/assets/home.jpg" width={200} height={150} alt="Gabriel Menezes" className="z-10 rounded-full mt-4" />
+        <p className="text-[20px] flex justify-center items-center">legenda 1</p>
+      </div>
+
+      <div className="m-4  grid grid-cols-2">
+      <p className="text-[20px] flex justify-center items-center">legenda 1</p>
+        <Image src="/assets/home.jpg" width={200} height={150} alt="Gabriel Menezes" className="z-10 rounded-full mt-4" />
+      </div>
+
+    </div>
+  </div>
     </section>
   );
 };
